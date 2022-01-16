6b42896a31d78036b50b2ac74559dd6f52748c38
Glynn Quelch <glynn.quelch@gmail.com>
2022-01-16 23:01:07 +0000

    House Keeping

--- src/QueryBuilder/QueryBuilderHandler.php
+++ src/QueryBuilder/QueryBuilderHandler.php
@@ -7,13 +7,13 @@ use Closure;
 use Throwable;
 use Pixie\Exception;
 use Pixie\Connection;
-use function mb_strlen;
 use Pixie\QueryBuilder\Raw;
 use Pixie\Hydration\Hydrator;
 use Pixie\QueryBuilder\JoinBuilder;
 use Pixie\QueryBuilder\QueryObject;
 use Pixie\QueryBuilder\Transaction;
 use Pixie\QueryBuilder\WPDBAdapter;
+use function mb_strlen;
 
 class QueryBuilderHandler
 {
