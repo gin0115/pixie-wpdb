63a72165614ab2928e2ae3146a0923958effdca0
Glynn Quelch <glynn.quelch@gmail.com>
2022-01-31 11:27:46 +0000

    House Keeping

--- src/QueryBuilder/QueryBuilderHandler.php
+++ src/QueryBuilder/QueryBuilderHandler.php
@@ -9,8 +9,9 @@ use Pixie\Binding;
 use Pixie\Exception;
 use Pixie\Connection;
 
-use Pixie\HasConnection;
+use function mb_strlen;
 
+use Pixie\HasConnection;
 use Pixie\JSON\JsonHandler;
 use Pixie\QueryBuilder\Raw;
 use Pixie\Hydration\Hydrator;
@@ -20,7 +21,6 @@ use Pixie\QueryBuilder\QueryObject;
 use Pixie\QueryBuilder\Transaction;
 use Pixie\QueryBuilder\WPDBAdapter;
 use Pixie\QueryBuilder\TablePrefixer;
-use function mb_strlen;
 
 class QueryBuilderHandler implements HasConnection
 {
@@ -1376,7 +1376,7 @@ class QueryBuilderHandler implements HasConnection
     /**
      * @return Connection
      */
-    public function getConnection()
+    public function getConnection(): Connection
     {
         return $this->connection;
     }
