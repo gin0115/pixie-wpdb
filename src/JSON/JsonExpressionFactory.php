6efb05ac2dd1d9d44370ac0c098b18a91ba1f8ae
Glynn Quelch <glynn.quelch@gmail.com>
2022-02-05 12:43:06 +0000

    added in extra unit tests for JsonExpressionFactory

--- src/JSON/JsonExpressionFactory.php
+++ src/JSON/JsonExpressionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Pixie\JSON;
 
+use Pixie\Exception;
 use Pixie\Connection;
 use Pixie\QueryBuilder\Raw;
 use Pixie\QueryBuilder\TablePrefixer;
@@ -29,17 +30,40 @@ class JsonExpressionFactory
     }
 
     /**
+     * Normalises the values passed as nodes
+     *
+     * @param mixed $nodes
+     * @return string
+     */
+    protected function normaliseNodes($nodes): string
+    {
+        // If its not an array, cast.
+        if (!is_array($nodes)) {
+            $nodes = [$nodes];
+        }
+
+        // Remove all none string.
+        $nodes = array_filter($nodes, function ($node): bool {
+            return is_string($node);
+        });
+
+        // If we have no nodes, throw.
+        if (count($nodes) === 0) {
+            throw new Exception("Only strings values may be passed as nodes.");
+        }
+
+        return \implode('.', $nodes);
+    }
+
+    /**
      * @param string          $column  The database column which holds the JSON value
      * @param string|string[] $nodes   The json key/index to search
      * @return \Pixie\QueryBuilder\Raw
      */
     public function extractAndUnquote(string $column, $nodes): Raw
     {
-
-        // Unpack any nodes.
-        if (is_array($nodes)) {
-            $nodes = \implode('.', $nodes);
-        }
+        // Normalise nodes.
+        $nodes = $this->normaliseNodes($nodes);
 
         // Add any possible prefixes to the key
         $column = $this->addTablePrefix($column, true);
